1841ff04cf7ef6c06a14de8d5adf6d11185af9c3
macymuhia <macymuhia@gmail.com>
2020-08-14 02:30:00 +0300

    Add theme files

---
+++ src/components/themes/DarkMode.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "DarkMode",
+
+  data() {
+    return {};
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style></style>
