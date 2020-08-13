bb45f3c11fc6b6dc1ea6f0078a9b707ee0c4bc33
macymuhia <macymuhia@gmail.com>
2020-08-14 02:35:23 +0300

    Add the landing page

---
+++ src/views/landing/LandingPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <TopBar></TopBar>
+    <BodyTemplate></BodyTemplate>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import TopBar from '@/components/navigation/TopBar';
+import BodyTemplate from '@/components/body/BodyTemplate';
+import Footer from '@/components/footer/Footer';
+
+export default {
+  name: "LandingPage",
+
+  components: {
+      TopBar,
+      BodyTemplate,
+      Footer
+  },
+
+  data() {
+    return {};
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
